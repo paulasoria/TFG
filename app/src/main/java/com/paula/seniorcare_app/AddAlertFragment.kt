2af9ab838d6421cb8b697ba57e712715b4179667
Paula <paulasoriaullan@usal.es>
2022-06-07 00:51:26 +0200

    alarm + calendar

--- app/src/main/java/com/paula/seniorcare_app/AddAlertFragment.kt
+++ app/src/main/java/com/paula/seniorcare_app/AddAlertFragment.kt
@@ -1,5 +1,6 @@
 package com.paula.seniorcare_app
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,9 +11,15 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputLayout
-import kotlinx.android.synthetic.main.activity_sign_up.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_add_alert.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import java.util.*
 
 class AddAlertFragment : Fragment() {
@@ -20,29 +27,36 @@ class AddAlertFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view:View = inflater.inflate(R.layout.fragment_add_alert, container, false)
         val setHourButton: Button = view.findViewById(R.id.setHourButton)
+        val setDateButton: Button = view.findViewById(R.id.setDateButton)
         val repetitionMenuTextView : AutoCompleteTextView = view.findViewById(R.id.repetitionMenuTextView)
         val saveAlertButton : Button = view.findViewById(R.id.saveAlertButton)
         val relativeTextInput : TextInputLayout = view.findViewById(R.id.relativeTextInput)
         val tagTextInput : TextInputLayout = view.findViewById(R.id.tagTextInput)
         val repetitionMenu : TextInputLayout = view.findViewById(R.id.repetitionMenu)
-        val l : TextView = view.findViewById(R.id.lunes)
-        val m : TextView = view.findViewById(R.id.martes)
-        val x : TextView = view.findViewById(R.id.miercoles)
-        val j : TextView = view.findViewById(R.id.jueves)
-        val v : TextView = view.findViewById(R.id.viernes)
-        val s : TextView = view.findViewById(R.id.sabado)
-        val d : TextView = view.findViewById(R.id.domingo)
+        val week = arrayOfNulls<TextView>(7)
+        week[0] = view.findViewById(R.id.lunes)
+        week[1] = view.findViewById(R.id.martes)
+        week[2] = view.findViewById(R.id.miercoles)
+        week[3] = view.findViewById(R.id.jueves)
+        week[4] = view.findViewById(R.id.viernes)
+        week[5] = view.findViewById(R.id.sabado)
+        week[6] = view.findViewById(R.id.domingo)
         val daysError : TextView = view.findViewById(R.id.daysError)
+        val dateError : TextView = view.findViewById(R.id.dateError)
+        var selectedItem : String? = null
 
         setHourButton.setOnClickListener {
             val calendar = Calendar.getInstance()
             TimePickerDialog(context, { picker, hour, minute ->
-                //Guardar datos alarma
-                //val id = UUID.randomUUID().toString()
-                //val alert = Alert(id, sender, receiver, tag, repetition, hour, minute, dayOfWeek, date, isOn)
-                //Poner datos en el texto del botón
                 setHourButton.text = "$hour:$minute"
-            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show()
+            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
+        }
+
+        setDateButton.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            DatePickerDialog(requireContext(), { picker, year, month, day ->
+                setDateButton.text = "$day/$month/$year"
+            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
         }
 
         val repetition = resources.getStringArray(R.array.repetition)
@@ -52,41 +66,84 @@ class AddAlertFragment : Fragment() {
             repetition
         )
         repetitionMenuTextView.setAdapter(adapter)
+        repetitionMenuTextView.setOnItemClickListener { parent, _, position, _ ->
+            selectedItem = parent.getItemAtPosition(position).toString()
+            if(selectedItem == "Semanal"){
+                weeklyLayout.visibility = View.VISIBLE
+                calendarLayout.visibility = View.INVISIBLE
+            } else if(selectedItem == "Eventual"){    //Eventual
+                weeklyLayout.visibility = View.INVISIBLE
+                calendarLayout.visibility = View.VISIBLE
+            }
+        }
 
-        changeColorSelectedDay(l)
-        changeColorSelectedDay(m)
-        changeColorSelectedDay(x)
-        changeColorSelectedDay(j)
-        changeColorSelectedDay(v)
-        changeColorSelectedDay(s)
-        changeColorSelectedDay(d)
+        week.forEach {
+            if (it != null) {
+                changeColorSelectedDay(it)
+            }
+        }
 
         saveAlertButton.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
             val relative = relativeTextInput.editText?.text.toString()
             val tag = tagTextInput.editText?.text.toString()
             val repetition = repetitionMenuTextView.text.toString()
-            if (relative.trim().isNotEmpty() && tag.trim().isNotEmpty() && repetition.trim().isNotEmpty() && !emptyDays(l,m,x,j,v,s,d)) {
-                //Guardar alerta
+            if (relative.trim().isNotEmpty() && tag.trim().isNotEmpty() && repetition.trim().isNotEmpty() && selectedItem != null) {
+                if(selectedItem == "Semanal" && !emptyDays(week)) {
+                    val id = UUID.randomUUID().toString()
+                    val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                    val daysOfWeek = getSelectedDays(week)
+                    lifecycleScope.launch {
+                        withContext(Dispatchers.IO) {
+                            createAlertInDatabase(db, id, uid, relative, tag, repetition, setHourButton.text.toString(), daysOfWeek, null)
+                        }
+                    }
+                    //Guardar alerta en la bbdd
+                } else {
+                    errorEmptyDays(week)
+                }
+
+                if(selectedItem == "Eventual" && !emptyDate()) {
+                    val id = UUID.randomUUID().toString()
+                    val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                    val date = setDateButton.text.toString()
+                    lifecycleScope.launch {
+                        withContext(Dispatchers.IO) {
+                            createAlertInDatabase(db, id, uid, relative, tag, repetition, setHourButton.text.toString(), null, date)
+                        }
+                    }
+                    //Guardar alerta como recordatorio eventual
+                } else {
+                    errorEmptyDate()
+                }
             } else {
                 emptyEditText(relativeTextInput)
                 emptyEditText(tagTextInput)
                 if(repetitionMenuTextView.text.toString().trim().isEmpty()){
                     repetitionMenu.error = getString(R.string.empty_field)
                 } else { repetitionMenu.error = null }
-                errorEmptyDays(l,m,x,j,v,s,d, daysError)
+                errorEmptyDays(week)
+                errorEmptyDate()
             }
-
-            if(weeklyLayout.visibility == View.INVISIBLE ){
-                weeklyLayout.visibility = View.VISIBLE
-                calendarLayout.visibility = View.INVISIBLE
-            } else {
-                weeklyLayout.visibility = View.INVISIBLE
-                calendarLayout.visibility = View.VISIBLE
-            }
-
         }
 
         return view
+    }
+
+    private suspend fun createAlertInDatabase(db: FirebaseFirestore, id: String, sender: String, receiver: String, tag: String, repetition: String, time: String, daysOfWeek: HashMap<String,Int>?, date: String?){
+        db.collection("alerts").document(id).set(
+            hashMapOf(
+                "id" to id,
+                "sender" to sender,
+                "receiver" to receiver,
+                "tag" to tag,
+                "repetition" to repetition,
+                "time" to time,
+                "daysOfWeek" to daysOfWeek,
+                "date" to date
+            )
+        ).await()
+        //Subcolecciones para sender y receiver
     }
 
     private fun changeColorSelectedDay(day: TextView){
@@ -105,27 +162,59 @@ class AddAlertFragment : Fragment() {
         } else { x.error = null }
     }
 
-    private fun emptyDays(l:TextView, m:TextView, x:TextView, j:TextView, v:TextView, s:TextView, d:TextView): Boolean {
-        return l.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                m.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                x.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                j.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                v.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                s.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-                d.currentTextColor == resources.getColor(R.color.super_light_grayish_blue)
+    private fun emptyDays(week:Array<TextView?>): Boolean {
+        var numEmpty = 0
+        week.forEach {
+            if (it != null) {
+                if(it.currentTextColor == resources.getColor(R.color.super_light_grayish_blue)){
+                    numEmpty += 1
+                }
+            }
+        }
+        return numEmpty == week.size
     }
 
-    private fun errorEmptyDays(l:TextView, m:TextView, x:TextView, j:TextView, v:TextView, s:TextView, d:TextView, daysError:TextView) {
-        if(l.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            m.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            x.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            j.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            v.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            s.currentTextColor == resources.getColor(R.color.super_light_grayish_blue) &&
-            d.currentTextColor == resources.getColor(R.color.super_light_grayish_blue)) {
+    private fun emptyDate(): Boolean {
+        if(setDateButton.text == "00/00/0000"){
+            return true
+        }
+        return false
+    }
+
+    private fun errorEmptyDays(week:Array<TextView?>) {
+        var numEmpty = 0
+        week.forEach {
+            if (it != null) {
+                if(it.currentTextColor == resources.getColor(R.color.super_light_grayish_blue)){
+                    numEmpty += 1
+                }
+            }
+        }
+        if(numEmpty == week.size){
             daysError.visibility = View.VISIBLE
         } else {
             daysError.visibility = View.INVISIBLE
         }
+    }
+
+    private fun errorEmptyDate(){
+        if(setDateButton.text == "00/00/0000"){
+            dateError.visibility = View.VISIBLE
+        } else {
+            dateError.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun getSelectedDays(week:Array<TextView?>): HashMap<String,Int> {
+        var selectedDays: HashMap<String,Int> = hashMapOf("l" to 0, "m" to 0, "x" to 0, "j" to 0, "v" to 0, "s" to 0, "d" to 0)
+
+        week.forEach {
+            if (it != null) {
+                if (it.currentTextColor == resources.getColor(R.color.turquoise)) {
+                    selectedDays[it.text.toString().toLowerCase()] = 1
+                }
+            }
+        }
+        return selectedDays
     }
 }
--- app/src/main/java/com/paula/seniorcare_app/model/Alert.kt
+++ app/src/main/java/com/paula/seniorcare_app/model/Alert.kt
@@ -1,6 +1,8 @@
 package com.paula.seniorcare_app.model
 
 import java.io.Serializable
+import java.util.*
+import kotlin.collections.ArrayList
 
 data class Alert(
     var id: String? = null,
@@ -8,9 +10,7 @@ data class Alert(
     var receiver: String? = null,
     var tag: String? = null,
     var repetition: String? = null, //weekly or eventually
-    var hour: Int,
-    var minute: Int,
-    var dayOfWeek: String? = null, //weekly         TYPE DAYOFWEEK¿?
-    var date: String? = null, //event
-    var isOn: Boolean   //Alarma activada o desactivada
+    var time: String? = null,       //weekly or eventually
+    var daysOfWeek: Array<Int>?, //weekly    1 on, 0 off
+    var date: String? = null                 //event     y,m,d
 ) : Serializable
